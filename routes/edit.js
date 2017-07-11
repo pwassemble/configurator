d752ea89e438d50b059b14a3b006eaf05a35ec83
tomayac <steiner.thomas@gmail.com>
2017-07-11 13:58:05 +0200

    Corrected editing bug for existing images.

--- routes/edit.js
+++ routes/edit.js
@@ -2,22 +2,6 @@ let express = require('express');
 let router = new express.Router();
 let images = require('../lib/images');
 
-let parseFileNames = function(obj) {
-  let fileKeys = ['companyLogo', 'homescreenIcon', 'heroImage'];
-  if (obj) {
-    fileKeys.forEach(function(key) {
-      let url = obj[key + 'Url'];
-      if (!url) {
-        obj[key] = '';
-        return;
-      }
-      let parts = url.split('/');
-      let index = parts.length - 1;
-      obj[key] = parts[index];
-    });
-  }
-};
-
 /* GET home page. */
 router.get('/', function(req, res) {
   const id = req.query.id;
@@ -28,7 +12,6 @@ router.get('/', function(req, res) {
     .getConfigJson(id)
     .then(function(configObj) {
       configObj.bucketName = id;
-      parseFileNames(configObj);
       res.render('index', {configObj: configObj});
     })
     .catch(function(error) {
